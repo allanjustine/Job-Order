82f497abc529671c2d481012f4277b613c908eb6
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-06-17 15:54:56 +0800

    backend web and welcome

--- server/routes/web.php
+++ server/routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json('Welcome to mobile legends. Five seconds to reach the battle field! SMASH THEM!', 501);
-});
+Route::view('/', 'welcome');
