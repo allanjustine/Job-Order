96ef0c488bac6e7c631621329e30de79fd2bca85
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-05-05 17:36:27 +0800

    Global loader fixed!

--- client/src/components/GlobalLoaders.tsx
+++ client/src/components/GlobalLoaders.tsx
@@ -1,7 +1,16 @@
-export default function GlobalLoader() {
+const GlobalLoader = () => {
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-50 h-screen bg-white justify-center items-center flex">
-      <span className="loader"></span>
+    <div
+      className={`fixed top-0 bottom-0 left-0 right-0 z-999 flex items-center justify-center bg-background`}
+    >
+      <iframe
+        src="https://smctdevt.github.io/smctloder/"
+        frameBorder="0"
+        allowFullScreen
+        style={{ width: "100%", height: "100%" }}
+      ></iframe>
     </div>
   );
-}
+};
+
+export default GlobalLoader;
