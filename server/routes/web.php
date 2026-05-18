13e7cab76f3c142551450d97c2d15d9ab828912a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-05-18 11:01:06 +0800

    added generate null jo

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
+use App\Models\JobOrder;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 Route::view('/', 'welcome');
+Route::get('generate-null-jo', function () {
+    $jobOrders = JobOrder::query()->whereNull('transaction_code')->get();
+
+    $jobOrders->each(function ($jobOrder) {
+        do {
+            $generated_code = "JO-" . Str::upper(Str::random(15));
+        } while (JobOrder::query()->where('transaction_code', "JO-{$generated_code}")->exists());
+
+        $jobOrder->update([
+            'transaction_code' => $generated_code
+        ]);
+    });
+});
