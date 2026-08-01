af71a5084ea2e48d846f740d6cbfa78450d174b3
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-08-01 08:35:31 +0800

    added error

--- client/src/components/NextSchedule.tsx
+++ client/src/components/NextSchedule.tsx
@@ -16,7 +16,7 @@ export default function NextSchedule({
       <div className="mt-2">
         <Label>Your Next Service Schedule is:</Label>
         <div className="flex items-center gap-2 mt-1">
-          <Input 
+          <Input
             placeholder="Date"
             type="date"
             className="w-80"
@@ -24,7 +24,7 @@ export default function NextSchedule({
             onChange={(e) => setNextScheduleDate(e.target.value)}
           />
           <p className="whitespace-nowrap">or</p>
-          <Input 
+          <Input
             placeholder="KMs"
             type="text"
             className="w-80"
@@ -35,15 +35,17 @@ export default function NextSchedule({
       </div>
 
       <div className="mt-4">
-        <Label>General Remarks:</Label>
-        <Input 
+        <Label>
+          General Remarks:<span className="text-red-500">*</span>
+        </Label>
+        <Input
           error={errors.generalRemarks}
           placeholder=""
           className="w-full mt-1"
           value={generalRemarks}
           onChange={(e) => setGeneralRemarks(e.target.value)}
         />
-         {errors.date && (
+        {errors.date && (
           <p className="text-red-500 text-xs mt-1">{errors.generalRemarks}</p>
         )}
       </div>
