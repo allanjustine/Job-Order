715c2a0111bb643951fe80018874e933c6937d6b
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-08-14 11:14:00 +0800

    added activity logs

--- server/app/Services/AreaManagerService.php
+++ server/app/Services/AreaManagerService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\AreaManager;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
@@ -49,6 +50,13 @@ class AreaManagerService
 
             $areaManager->users()->attach($request->user_ids);
 
+            $user_count = count($request->user_ids);
+
+            activity()
+                ->causedBy(Auth::user())
+                ->performedOn($areaManager)
+                ->log("Added area manager {$areaManager->name} with {$user_count} branches.");
+
             return $areaManager;
         });
     }
@@ -62,12 +70,24 @@ class AreaManagerService
 
             $areaManager->users()->sync($request->user_ids);
 
+            $user_count = count($request->user_ids);
+
+            activity()
+                ->causedBy(Auth::user())
+                ->performedOn($areaManager)
+                ->log("Added {$user_count} branches to {$areaManager->name}.");
+
             return $areaManager;
         });
     }
 
     public function delete($areaManager)
     {
+        activity()
+            ->causedBy(Auth::user())
+            ->performedOn($areaManager)
+            ->log("Deleted area manager {$areaManager->name}.");
+
         return $areaManager->delete();
     }
 }
