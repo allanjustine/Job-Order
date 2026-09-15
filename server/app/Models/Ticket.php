d94d33409ca2a9f1215d608459a404ffcedef1b1
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-09-15 09:47:46 +0800

    added ticket model

---
+++ server/app/Models/Ticket.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TicketStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'status'    => TicketStatus::class,
+            'edited_at' => 'datetime',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function editedBy()
+    {
+        return $this->belongsTo(User::class, 'edited_by');
+    }
+
+    public function notes()
+    {
+        return $this->hasMany(TicketNote::class);
+    }
+
+    public function attachments()
+    {
+        return $this->hasMany(TicketAttachment::class);
+    }
+
+    public function changeRequests()
+    {
+        return $this->hasMany(TicketChangeRequest::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(TicketCategory::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(TicketBrand::class);
+    }
+
+    public function jobOrder()
+    {
+        return $this->belongsTo(JobOrder::class);
+    }
+
+    public function ticketCategory()
+    {
+        return $this->belongsTo(TicketCategory::class);
+    }
+
+    public function ticketBrand()
+    {
+        return $this->belongsTo(TicketBrand::class);
+    }
+}
