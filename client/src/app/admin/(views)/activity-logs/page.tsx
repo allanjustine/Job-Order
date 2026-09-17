c64a05c334ca694a49c24d57fbb28efa82a5b445
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-09-17 16:13:08 +0800

    permission access

--- client/src/app/admin/(views)/activity-logs/page.tsx
+++ client/src/app/admin/(views)/activity-logs/page.tsx
@@ -6,6 +6,7 @@ import Input from "@/components/ui/input";
 import { PER_PAGE_OPTIONS } from "@/constants/perPageOptipns";
 import useFetch from "@/hooks/useFetch";
 import withAuthPage from "@/lib/hoc/with-auth-page";
+import { ADMIN_ACCESS } from "@/lib/permissions";
 import { format, formatDistanceToNowStrict } from "date-fns";
 import { Search, SearchSlash } from "lucide-react";
 import DataTable from "react-data-table-component";
@@ -175,4 +176,4 @@ const ActivityLogs = () => {
   );
 };
 
-export default withAuthPage(ActivityLogs);
+export default withAuthPage(ActivityLogs, ADMIN_ACCESS);
