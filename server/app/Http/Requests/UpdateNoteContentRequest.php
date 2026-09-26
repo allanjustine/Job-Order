034e6b58135f30423624826d9294915d0b4c36d6
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-09-26 11:00:11 +0800

    revert admin

--- server/app/Http/Requests/UpdateNoteContentRequest.php
+++ server/app/Http/Requests/UpdateNoteContentRequest.php
@@ -12,7 +12,7 @@ class UpdateNoteContentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return Auth::check() && (Auth::user()->isAdmin() || Auth::user()->isAccounting());
+        return Auth::check() && Auth::user()->isAdmin();
     }
 
     /**
