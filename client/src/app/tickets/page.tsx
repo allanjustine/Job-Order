e21f6432807eb580888838c783efcd3cee201bd0
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-09-26 09:46:39 +0800

    ticket page only tickets_access can access

--- client/src/app/tickets/page.tsx
+++ client/src/app/tickets/page.tsx
@@ -2,7 +2,7 @@
 
 import TicketBaseContent from "@/components/tickets/base";
 import withAuthPage from "@/lib/hoc/with-auth-page";
-import { ADMIN_ACCESS } from "@/lib/permissions";
+import { TICKETS_ACCESS } from "@/lib/permissions";
 
 const Tickets = () => {
   return (
@@ -14,4 +14,4 @@ const Tickets = () => {
   );
 };
 
-export default withAuthPage(Tickets, ADMIN_ACCESS);
+export default withAuthPage(Tickets, TICKETS_ACCESS);
